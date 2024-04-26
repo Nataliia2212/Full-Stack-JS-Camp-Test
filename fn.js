9b54a366b3a0796c82954980eafb4c484af6688e
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-04-27 00:42:51 +0300

    clear

--- fn.js
+++ fn.js
@@ -1,16 +1,3 @@
-const salaries1 = {
-  Manager: { salary: 1000, tax: "10%" },
-  Designer: { salary: 600, tax: "30%" },
-  Artist: { salary: 1500, tax: "15%" },
-};
-
-const team1 = [
-  { name: "Misha", specialization: "Manager" },
-  { name: "Max", specialization: "Designer" },
-  { name: "Vova", specialization: "Designer" },
-  { name: "Leo", specialization: "Artist" },
-];
-
 function calculateTotalSalary({ salary, tax }) {
   const totalSalary = (salary * 100) / (100 - Number.parseInt(tax));
   return totalSalary;
@@ -47,21 +34,3 @@ function calculateTeamFinanceReport(salaries, team) {
 
   return financeReport;
 }
-
-const financeReport1 = calculateTeamFinanceReport(salaries1, team1);
-
-console.log(JSON.stringify(financeReport1));
-
-const salaries2 = {
-  TeamLead: { salary: 1000, tax: "99%" },
-  Architect: { salary: 9000, tax: "34%" },
-};
-const team2 = [
-  { name: "Alexander", specialization: "TeamLead" },
-  { name: "Gaudi", specialization: "Architect" },
-  { name: "Koolhas", specialization: "Architect" },
-  { name: "Foster", specialization: "Architect" },
-  { name: "Napoleon", specialization: "General" },
-];
-const financeReport2 = calculateTeamFinanceReport(salaries2, team2);
-console.log(JSON.stringify(financeReport2));
