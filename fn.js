43575104606b8325db5cc69fa539a850fc8b2f23
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-04-27 00:40:50 +0300

    finash

--- fn.js
+++ fn.js
@@ -13,44 +13,55 @@ const team1 = [
 
 function calculateTotalSalary({ salary, tax }) {
   const totalSalary = (salary * 100) / (100 - Number.parseInt(tax));
-  return Math.round(totalSalary);
+  return totalSalary;
 }
 
 function totalBudgetTeam(team, salaries) {
-  return team.reduce((total, member) => {
+  const totalSalaryWithTax = team.reduce((total, member) => {
     const memberSalary = salaries[member.specialization];
+    if (!memberSalary) {
+      return total;
+    }
     return total + calculateTotalSalary(memberSalary);
   }, 0);
+
+  return totalSalaryWithTax;
 }
-
-function nameOfSpecialization(specialization) {
-  return "totalBudget" + specialization;
-}
-
-// console.log(nameOfSpecialization("Manager"));
-
-// console.log(totalBudgetTeam(team1, salaries1));
 
 function calculateTeamFinanceReport(salaries, team) {
   const financeReport = {};
   financeReport.totalBudgetTeam = totalBudgetTeam(team, salaries);
-  const keys = Object.keys(salaries);
-  const listOfSpecialization = keys.map((key) => {
-    console.log(financeReport[nameOfSpecialization(key)]);
-    if (financeReport[nameOfSpecialization(key)]) {
-      financeReport[nameOfSpecialization(key)] =
-        financeReport[nameOfSpecialization(key)] +
-        calculateTotalSalary(salaries[key]);
-    } else {
-      financeReport[nameOfSpecialization(key)] = calculateTotalSalary(
-        salaries[key]
-      );
+
+  team.forEach(({ specialization }) => {
+    if (salaries[specialization]) {
+      const budgetKey = "totalBudget" + specialization;
+      financeReport[budgetKey] =
+        (financeReport[budgetKey] || 0) +
+        calculateTotalSalary(salaries[specialization]);
     }
   });
+
+  Object.keys(financeReport).forEach((key) => {
+    financeReport[key] = Math.trunc(financeReport[key]);
+  });
+
   return financeReport;
 }
 
 const financeReport1 = calculateTeamFinanceReport(salaries1, team1);
 
-console.log(financeReport1);
 console.log(JSON.stringify(financeReport1));
+
+const salaries2 = {
+  TeamLead: { salary: 1000, tax: "99%" },
+  Architect: { salary: 9000, tax: "34%" },
+};
+const team2 = [
+  { name: "Alexander", specialization: "TeamLead" },
+  { name: "Gaudi", specialization: "Architect" },
+  { name: "Koolhas", specialization: "Architect" },
+  { name: "Foster", specialization: "Architect" },
+  { name: "Napoleon", specialization: "General" },
+];
+const financeReport2 = calculateTeamFinanceReport(salaries2, team2);
+console.log(JSON.stringify(financeReport2));
